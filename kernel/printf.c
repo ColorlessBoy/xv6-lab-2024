f2922ea7080e34c910b4c2ce0aef45d69cd6f5e6
penglingwei <penglingwei@zju.edu.cn>
2025-03-24 12:12:30 +0800

    backtrace

--- kernel/printf.c
+++ kernel/printf.c
@@ -165,6 +165,7 @@ panic(char *s)
   pr.locking = 0;
   printf("panic: ");
   printf("%s\n", s);
+  backtrace();
   panicked = 1; // freeze uart output from other CPUs
   for(;;)
     ;
@@ -175,4 +176,16 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void
+backtrace(void) 
+{
+  printf("backtrace:\n");
+  uint64 fp = r_fp();
+  uint64 start = PGROUNDDOWN(fp) + PGSIZE;
+  while (fp < start) {
+    printf("%p\n", (uint64 *)*(uint64 *)(fp - 8));
+    fp = *(uint64 *)(fp - 16);
+  }
 }
